459f68ac057ddb04546cba05d2ac6236842441bd
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-04-07 08:43:22 +0900

    Update test-script for UTF8

--- t_filesystemobject.lua
+++ t_filesystemobject.lua
@@ -1,10 +1,17 @@
-local ole = require('nyole')
-local fsObj = ole.create_object("Scripting.FileSystemObject")
-local fd = fsObj:OpenTextFile(arg[0])
-if fd then
-    while not fd.AtEndOfStream do
-        local line = fd:ReadLine()
-        print(line)
+
+function typefile(fsObj)
+    local fd = fsObj:OpenTextFile(arg[0])
+    if fd then
+        while not fd.AtEndOfStream do
+            local line = fd:ReadLine()
+            print(line)
+        end
+        fd:Close()
     end
-    fd:Close()
 end
+
+local ole = require('nyole')
+print("Try with ANSI")
+typefile(ole.create_object("Scripting.FileSystemObject"))
+print("Try with UTF8")
+typefile(ole.create_object_utf8("Scripting.FileSystemObject"))
--- t_specialfolders.lua
+++ t_specialfolders.lua
@@ -1,4 +1,6 @@
 local ole=require('nyole')
 local objShell = ole.create_object("WScript.Shell")
-print(objShell.SpecialFolders("Desktop"):Item(0))
+print("ANSI:",objShell.SpecialFolders("Desktop"):Item(0))
 
+objShell = ole.create_object_utf8("WScript.Shell")
+print("UTF8:",objShell.SpecialFolders("Desktop"):Item(0))

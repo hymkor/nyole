45977464e4eece7f03f0857b58bca9b72293dd3c
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-04-17 19:41:23 +0900

    Set dll's fullpath to variable 'dll'(ANSI) and 'dll_utf8'(UTF8)

--- nyole.cpp
+++ nyole.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <windows.h>
 
 extern "C"{
 #  include "lua.h"
@@ -8,6 +9,10 @@ extern "C"{
 
 // #define DBG(s) (s)
 #define DBG(s)
+
+static wchar_t modulePath[ _MAX_PATH ];
+static char modulePathAnsi[ _MAX_PATH ];
+static char modulePathUtf8[ _MAX_PATH ];
 
 extern int com_create_object( lua_State *L );
 extern int com_create_object_utf8( lua_State *L );
@@ -28,5 +33,21 @@ extern "C" int luaopen_nyole(lua_State *L)
     lua_setfield(L,-2,"get_active_object_utf8");
     lua_pushcfunction(L,com_const_load);
     lua_setfield(L,-2,"const_load");
+    lua_pushstring(L,modulePathAnsi);
+    lua_setfield(L,-2,"dll");
+    lua_pushstring(L,modulePathUtf8);
+    lua_setfield(L,-2,"dll_utf8");
     return 1;
+}
+
+
+BOOL APIENTRY DllMain (HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    GetModuleFileNameW((HMODULE) hModule,
+            modulePath,
+            sizeof(modulePath)/sizeof(wchar_t));
+    WideCharToMultiByte(CP_ACP,0,modulePath,-1,modulePathAnsi,sizeof(modulePathAnsi)/sizeof(char)-1,NULL,NULL);
+    WideCharToMultiByte(CP_UTF8,0,modulePath,-1,modulePathUtf8,sizeof(modulePathUtf8)/sizeof(char)-1,NULL,NULL);
+
+    return TRUE;
 }
---
+++ t_version.lua
@@ -0,0 +1,3 @@
+local ole=require('nyole')
+print(ole.dll)
+print(ole.dll_utf8)
